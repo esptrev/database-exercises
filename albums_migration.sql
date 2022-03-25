595522dc39353772b35a73929df3b50db1875ba3
Trevor Esparza <trevoresparza30@gmail.com>
2022-03-25 06:18:40 -0500

    first table

---
+++ albums_migration.sql
@@ -0,0 +1,9 @@
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR (50),
+    name VARCHAR (50) NOT NULL,
+    release_date DATE,
+    sales FLOAT UNSIGNED,
+    genre VARCHAR (50),
+    PRIMARY KEY (id)
+);
