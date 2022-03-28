e2797c6723c99e656554369106b5fbe1938be7a6
Trevor Esparza <trevoresparza30@gmail.com>
2022-03-28 09:59:11 -0500

    slightly different albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -7,3 +7,12 @@ CREATE TABLE albums(
     genre VARCHAR (50),
     PRIMARY KEY (id)
 );
+
+CREATE TABLE `albums2` (
+     `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     `album_name` varchar(100) NOT NULL,
+     `artist_name` varchar(100) NOT NULL,
+     `release_date` int NOT NULL,
+     `sales` decimal(10,6) NOT NULL,
+     `genre` varchar(50) NOT NULL
+);
