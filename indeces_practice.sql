fb3f707d114eb1088ba641b0703ef9827b6e71b0
Trevor Esparza <trevoresparza30@gmail.com>
2022-04-01 09:37:37 -0500

    bonus

--- indeces_practice.sql
+++ indeces_practice.sql
@@ -1,69 +1,72 @@
-create database if not exists elixir_db;
-
-use elixir_db;
-
-create table if not exists players
-(
-    id int unsigned not null auto_increment PRIMARY KEY,
-    first_name varchar(75),
-    last_name varchar(75),
-    team varchar(50),
-    position varchar(50)
-);
-
-DROP TABLE IF EXISTS players;
-
-create table if not exists players
-(
-    id int unsigned not null auto_increment,
-    first_name varchar(75),
-    last_name varchar(75),
-    team varchar(50),
-    position varchar(50),
-    PRIMARY KEY (id)
-);
-
-create table if not exists players
-(
-    id int unsigned not null,
-    first_name varchar(75),
-    last_name varchar(75),
-    team varchar(50),
-    position varchar(50)
-);
-
-ALTER TABLE players
-ADD PRIMARY KEY (id);
-
-DESCRIBE players;
-
-INSERT INTO players(FIRST_NAME, LAST_NAME, TEAM, POSITION)
-VALUES ('JOE', 'BURROW','CINCI','QB'),
-       ('PATRICK', 'MAHOMES','KC','QB');
-
-SELECT *
-FROM players;
-
-CREATE TABLE IF NOT EXISTS teams
-(
-    id int  unsigned not null auto_increment primary key,
-    city VARCHAR(50),
-    name VARCHAR(50) UNIQUE
-);
-
-DESCRIBE teams;
-
-INSERT INTO teams(city, name)
-VALUES ('CINCI', 'BENGALS'),
-       ('KC', 'CHIEFS');
-
-SELECT *
-FROM teams;
-
+# create database if not exists elixir_db;
+#
+# use elixir_db;
+#
+# create table if not exists players
+# (
+#     id int unsigned not null auto_increment PRIMARY KEY,
+#     first_name varchar(75),
+#     last_name varchar(75),
+#     team varchar(50),
+#     position varchar(50)
+# );
+#
+# DROP TABLE IF EXISTS players;
+#
+# create table if not exists players
+# (
+#     id int unsigned not null auto_increment,
+#     first_name varchar(75),
+#     last_name varchar(75),
+#     team varchar(50),
+#     position varchar(50),
+#     PRIMARY KEY (id)
+# );
+#
+# create table if not exists players
+# (
+#     id int unsigned not null,
+#     first_name varchar(75),
+#     last_name varchar(75),
+#     team varchar(50),
+#     position varchar(50)
+# );
+#
+# ALTER TABLE players
+# ADD PRIMARY KEY (id);
+#
+# DESCRIBE players;
+#
+# INSERT INTO players(FIRST_NAME, LAST_NAME, TEAM, POSITION)
+# VALUES ('JOE', 'BURROW','CINCI','QB'),
+#        ('PATRICK', 'MAHOMES','KC','QB');
+#
+# SELECT *
+# FROM players;
+#
+# CREATE TABLE IF NOT EXISTS teams
+# (
+#     id int  unsigned not null auto_increment primary key,
+#     city VARCHAR(50),
+#     name VARCHAR(50) UNIQUE
+# );
+#
+# DESCRIBE teams;
+#
 # INSERT INTO teams(city, name)
-# VALUES ('DALLAS', 'BENGALS');
+# VALUES ('CINCI', 'BENGALS'),
+#        ('KC', 'CHIEFS');
+#
+# SELECT *
+# FROM teams;
+#
+# # INSERT INTO teams(city, name)
+# # VALUES ('DALLAS', 'BENGALS');
+#
+# SELECT *
+# FROM teams;
 
-SELECT *
-FROM teams;
 
-
+drop database if exists elixir_join_db;
+create database if not exists elixir_join_db;
+use elixir_join_db;
--- join_exercises.sql
+++ join_exercises.sql
@@ -1,9 +1,60 @@
 use trevor;
 
-select dept_name, CONCAT(employees.first_name, ' ',employees.last_name) as Name
-from trevor.employees
-         JOIN departments
-              ON departments.dept_no = de.dept_no
-         JOIN dept_manager as dm
-              ON dm.emp_no = employees.emp_no
+select dept_name AS Dept,
+       CONCAT(e.first_name, ' ',e.last_name) as Manager
+from trevor.departments AS d
+    join dept_manager AS dm on d.dept_no = dm.dept_no
+    join employees e on dm.emp_no = e.emp_no
+where to_date > NOW();
+
+
+
+select dept_name AS Dept,
+       CONCAT(e.first_name, ' ',e.last_name) as Manager
+from trevor.departments AS d
+         join dept_manager AS dm on d.dept_no = dm.dept_no
+         join employees e on dm.emp_no = e.emp_no
+where to_date > NOW()
+    AND gender = 'F';
+
+
+
+SELECT title, count(*)
+FROM titles
+    JOIN employees AS e
+        on titles.emp_no = e.emp_no
+    JOIN dept_emp AS de
+        on e.emp_no = de.emp_no
+WHERE dept_no = 'd009'
+    AND de.to_date > NOW()
+    AND titles.to_date > NOW()
+GROUP BY title;
+
+
+
+select dept_name AS Dept,
+       CONCAT(e.first_name, ' ',e.last_name) as Manager,
+       s.salary
+from trevor.departments AS d
+         join dept_manager AS dm on d.dept_no = dm.dept_no
+         join employees e on dm.emp_no = e.emp_no
+         join salaries s on dm.emp_no = s.emp_no
+where dm.to_date > NOW()
+    AND s.to_date > NOW();
+
+
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee,
+       dept_name,
+       CONCAT(emp.first_name, ' ',emp.last_name) as Manager
+FROM employees AS e
+    JOIN dept_emp ON dept_emp.emp_no = e.emp_no
+    JOIN departments AS d ON dept_emp.dept_no = d.dept_no
+    JOIN dept_manager AS dm ON d.dept_no = dm.dept_no
+    JOIN employees AS emp ON dm.emp_no = emp.emp_no
+
+WHERE dept_emp.to_date > NOW()
+        AND dm.to_date > NOW();
+
+
 
